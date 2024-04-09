6fc90bba66b5607b5a5610a07b9b93eeb6db7cbe
RomeoSierra117 <rey2146221@gmail.com>
2024-04-09 15:10:51 -0500

    First Commit V2

--- GitTest/src/main/java/org/example/Main.java
+++ GitTest/src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+        System.out.println("Im a programmer now lol");
+        System.out.println("Gonna make that bread!");
 }
